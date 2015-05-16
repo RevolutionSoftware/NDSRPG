c64bdcfac6544624fbaaa3a6a18359bebea99b0b
gpadd <unknown.areth@gmail.com>
2015-05-16 20:17:25 +0200

    Prettifying comments

--- source/main.c
+++ source/main.c
@@ -1,8 +1,7 @@
- //
-// Leccion 1
-// Carga de fondos "tileados" desde FAT
-// Ejemplo por NightFox
-//
+/* 
+ * main.c
+ * Authors: Chickendude, add
+ */
 
 // Includes NDS
 #include <nds.h>	// Main NDS equates
@@ -10,12 +9,12 @@
 #include "font.h"
 #include "tiles.h"
 
-// Each map entry is apparently 2 bytes, since a tileset
-// .. can hold up to 1024 tiles
-// Map is currently exactly same size as screen
-// 32 8x8 tiles wide, 24 tiles tall
-u16 map[] = {
-	0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,
+/* Each map entry is apparently 2 bytes, since a tileset can hold up
+ * to 1024 tiles. Map is currently exactly same size as screen,
+ * 32 8x8 tiles wide, 24 tiles tall.
+ */
+static u16 map[] = {
+    0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,
 	2,3,6,7,2,3,2,3,2,3,2,3,2,3,2,3,2,3,6,7,2,3,2,3,2,3,2,3,2,3,2,3,
 	0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,0,1,4,5,0,1,0,1,0,1,0,1,0,1,0,1,
 	2,3,6,7,2,3,2,3,2,3,2,3,2,3,2,3,2,3,6,7,2,3,2,3,2,3,2,3,2,3,2,3,
@@ -43,37 +42,41 @@ u16 map[] = {
 
 
 int main(void) {
+    int bg0;
 	consoleDemoInit();
 	iprintf("NDSRPG:\n");
 	iprintf("4-bit color tilemap");
 
-// NDS has nine memory banks, banks 0-4
-// Use mode 0. Mode 0 is for tilebased sprites, called "text" mode
-// Other modes have options for rotation, scaling, and bitmap display
-// You have access to 4 backgrounds in mode 0
-	videoSetMode(MODE_0_2D | DISPLAY_BG0_ACTIVE);	// Set mode 0 in 2D mode (not 3D)
+/* NDS has nine memory banks, banks 0-4
+ *  Use mode 0. Mode 0 is for tilebased sprites, called "text" mode
+ * Other modes have options for rotation, scaling, and bitmap display
+ * You have access to 4 backgrounds in mode 0
+ */
+	videoSetMode(MODE_0_2D | DISPLAY_BG0_ACTIVE);   // Set mode 0 in 2D mode (not 3D)
     vramSetBankA(VRAM_A_MAIN_BG);					// There are nine memory banks, use memory bank A
-// Initializes the background:
-//	"Text" essentially means the same thing as "Tile"
-//	Text4bpp means our tiles are 4bpp
-// static int bgInit (	int layer (0-3), BgType type, BgSize size,
-//						int mapBase (2kb offset in VRAM), int tileBase (16kb offset in VRAM) )
-// There are other sizes available other than 256x256, eg. 512x256 and 512x512
-//	I think that's what we'll use for scrolling our maps
-	int bg0 = bgInit(0, BgType_Text4bpp, BgSize_T_256x256, 0,1);
+    /* Initializes the background:
+     * 	"Text" essentially means the same thing as "Tile"
+     * Text4bpp means our tiles are 4bpp
+     * static int bgInit (int layer (0-3), BgType type, BgSize size,
+     *                    int mapBase (2kb offset in VRAM), int tileBase (16kb offset in VRAM))
+     * There are other sizes available other than 256x256, eg. 512x256 and 512x512
+     * I think that's what we'll use for scrolling our maps
+     */
+	bg0 = bgInit(0, BgType_Text4bpp, BgSize_T_256x256, 0,1);
 
-// use DMA to copy data over
-// bgGetGfxPtr gets the uses the id from bgInit (or bgInitSub, which works with the bottom screen)
+/* use DMA to copy data over
+ * bgGetGfxPtr gets the uses the id from bgInit (or bgInitSub, which works with the bottom screen)
+ */
 	dmaCopy(tilesTiles, bgGetGfxPtr(bg0), tilesTilesLen);
 	dmaCopy(map, bgGetMapPtr(bg0), sizeof(map));
 	dmaCopy(tilesPal, BG_PALETTE, 256*2);
 	while(1) {
-		swiWaitForVBlank();
+        swiWaitForVBlank();
 		scanKeys();
 		// Exit if Start was pressed
-		if (keysDown()&KEY_START) break;
+		if (keysDown()&KEY_START)
+            break;
 	}
 
 	return 0;
 }
- 

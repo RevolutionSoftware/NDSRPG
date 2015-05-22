a2eb805d2cb01293db3f66f7703a154802d43c0e
gpadd <unknown.areth@gmail.com>
2015-05-22 20:51:09 +0200

    Global indentation on files in need of proper indentation. Reason for them being there was a faulty configuration (that I have payed no attention to before) in my editor.

--- source/tilemap.h
+++ source/tilemap.h
@@ -2,13 +2,13 @@
 #define TILEMAP_H
 
 typedef struct {
-	bool isPassable;	// 0 = false, 1 = true
-	u16 textId;			// -1 = no text, 0+ = string to display when activated with [A]
-	u8 mapId;			// -1 = no jump, otherwise map to switch to
-	u16 mapX;
-	u16 mapY;
-	u16 playerX;		// player's coordinates in the new map
-	u16 playerY;
+    bool isPassable;	// 0 = false, 1 = true
+    u16 textId;			// -1 = no text, 0+ = string to display when activated with [A]
+    u8 mapId;			// -1 = no jump, otherwise map to switch to
+    u16 mapX;
+    u16 mapY;
+    u16 playerX;		// player's coordinates in the new map
+    u16 playerY;
 }tile_t;
 
 void tilemap(u16 map[], int w, int h, int x, int y);

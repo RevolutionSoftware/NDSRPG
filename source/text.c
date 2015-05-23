9d23388b8f39f38e8ceda9d9d717f7c08d93499b
chickendude <pokemaster103@gmail.com>
2015-05-23 17:32:55 +0800

    Fixed some bugs with the text routine (\n and spaces)

--- source/text.c
+++ source/text.c
@@ -8,18 +8,18 @@
 
 // text is defined as a tilemap
 void putString(char *text) {
-    int i;
+	int i;
 
 #ifndef USE_STRLEN
-    int size = 0;
-    // get size of string (sizeof won't work for pointers)
-    while(text[size] != '\0') {
-       size++;
-    }
+	int text_length = 0;
+	// get size of string (sizeof won't work for pointers)
+	while(text[text_length] != '\0') {
+		text_length++;
+	}
 #endif
 
 #ifdef USE_STRLEN
-    size_t text_length = strlen(text); // string.h
+	size_t text_length = strlen(text); // string.h
 #endif // USE_STRLEN
 
     // address of tilemap
@@ -28,36 +28,33 @@ void putString(char *text) {
     // Draw the message on the screen.
     int x = 0, y = 0;
 
-#ifdef USE_STRLEN
-    for (i = 0; i < text_length; i++) {
-#else
-    for(i = 0; i < size; i++) {
-#endif // USE_STRLEN
+	for (i = 0; i < text_length; i++) {
+		// Check for special characters (\n, etc.)
+		if (text[i] == '\n') {
+			x = 0;
+			y++;
+			continue;
+		}
+		else if (text[i] == ' ') {
+			int j = i+1;
 
-        // Check for special characters (\n, etc.)
-        if (text[i] == '\n') {
-            x = 0;
-            y++;
-        }
-        else if (text[i] == ' ') {
-            int j = i++;
+			while(text[j] != ' ' && text[j] != '\0') {
+				j++;
+			}
+			if(j-i+x > 32) {	// j-i = number of characters to next space
+								// .. x + characters to next space
+				i++;			// skip the space
+				x = 0;			// and move to a new line
+				y++;
+			}
+		}
+		// copy tile to map
+		sub_map[y*32+x] = text[i]-' ';
+		x++;
 
-            while(text[j] != ' ' && text[j] != '\0') {
-                j++;
-            }
-            if(j-i+x > 32) {		// j-i = number of characters to next space
-                                        // .. x + characters to next space
-                i++;			// skip the space
-                x = 0;			// and move to a new line
-                y++;
-            }
-        }
-        // copy tile to map
-        sub_map[y*32+x] = text[i]-' ';
-        x++;
-        // Check for new line
-        x %= 32;	// screen is 32 tiles wide
-        if(!x)
-            y++;
-    }
+		// Check for new line
+		x %= 32;	// screen is 32 tiles wide
+		if(!x)
+			y++;
+	}
 }

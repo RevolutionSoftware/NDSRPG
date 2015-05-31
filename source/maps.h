b14e3c11709b170f274a9c46141086fcc68d2c8d
chickendude <pokemaster103@gmail.com>
2015-05-31 13:06:41 +0800

    Some more changes to the font and textbox.

--- source/maps.h
+++ source/maps.h
@@ -6,5 +6,5 @@ map_t map_list[2] = {{map0,map0_tiledata,map0_obj_data,32,32},
 					{map1,map1_tiledata,map1_obj_data,64,64}};
 
 // map number, mapx, mapy, playerx, playery
-u16 map_change_list[2][5] = {{1,0,0,1,2},
+u16 map_change_list[2][5] = {{1,0,0,1,1},
 						{0,10,5,16,10}};

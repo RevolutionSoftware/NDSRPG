98e3afd35a8ecab2b46f1f0159a55e73b7ccc52f
gpadd <unknown.areth@gmail.com>
2015-06-03 17:30:46 +0200

    Removed alignment enum and changed speed enum name to e_speed

--- source/text.h
+++ source/text.h
@@ -1,17 +1,11 @@
 #ifndef TEXT_H
 #define TEXT_H
 
-typedef enum speed {
+typedef enum {
     D_NONE,
     D_FAST,
     D_SLOW
-} speed;
-
-typedef enum alignment {
-    A_NONE,
-    A_CENTER,
-    A_BOTTOM
-} alignment;
+} e_speed;
 
 typedef struct {
     int x;

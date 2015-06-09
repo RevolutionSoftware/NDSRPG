f3e3618141776f8acb9d9eec4034f47c3803e56d
chickendude <pokemaster103@gmail.com>
2015-06-09 18:46:55 +0800

    Added menus.h

---
+++ source/texts/menus.h
@@ -0,0 +1,26 @@
+// main menu when pressing Y
+char *menu_Y	=	"\1Items\n"
+					"\1Equipment\n"
+					"\1Stats\n"
+					"\1Options\n"
+					"\1Save\n"
+					"\1Exit";
+
+// Items menu
+char *menu_items	=	"Sorry, we haven't programmed items into the game yet! Please check back in about 3 months ;)";
+
+// Equipment menu
+char *menu_equipment	=	"It uh.. kinda appears that you are naked. You should really find some armor.";
+
+// Stats menu
+char *menu_stats	=	"Stats:\n"
+						"HP: 1,000,000\n"
+						"ATK: Super STRoNG\n"
+						"DEF: None (naked)\n"
+						"Type: Super Badass";
+
+// Options menu
+char *menu_options	=	"What do you need options for? Our game is perfect as-is!";
+
+// Save menu
+char *menu_save		=	"There is no saving you! MwahAHahHAH!";

07af3d8718f9bc3448de481a7ec57bb6a069a2d4
chickendude <pokemaster103@gmail.com>
2015-06-20 19:43:16 +0800

    Pushing what i've done so far.

--- source/battle.c
+++ source/battle.c
@@ -7,15 +7,19 @@
 #include "constants.h"
 
 // sprites
-#include "battles.h"
+#include "battlePlayer.h"
+//#include "battleEnemy.h"
+//#include "battle8x8.h"
 
 void startBattle() {
 	int i;
-	dmaCopy(battlesPal, SPRITE_PALETTE, battlesPalLen);
+	dmaCopy(battlePlayerPal, SPRITE_PALETTE, battlePlayerPalLen);
+	
+	loadEnemies();
 	
 	for(i = 0; i < 3; i++) {
 		b_player[i].id = 0;		// 0 = first player (for now the filler sprites i've drawn
-		b_player[i].action = A_STAND;
+		b_player[i].state = A_STAND;
 		b_player[i].frame = 0;	// animation counter
 		b_player[i].x = 180;
 		b_player[i].y = i*35+20;
@@ -24,18 +28,33 @@ void startBattle() {
 	while(running) {
 		delay(1);
 		drawPlayers();
+		drawEnemies();
 		scanKeys();
 		if(keysDown()&(KEY_A|KEY_B))
 			running = false;
 		if(keysDown()&KEY_DOWN)
-			b_player[0].action = A_KICK_L;
-		if(keysDown()&KEY_UP)
-			b_player[0].action = A_KICK_H;
-		if(keysDown()&KEY_RIGHT)
-			b_player[0].action = A_PUNCH_R;
-		if(keysDown()&KEY_LEFT)
-			b_player[0].action = A_PUNCH_L;
+			b_player[0].state = A_KICK_L;
+		else if(keysDown()&KEY_UP)
+			b_player[0].state = A_KICK_H;
+		else if(keysDown()&KEY_RIGHT)
+			b_player[0].state = A_PUNCH_R;
+		else if(keysDown()&KEY_LEFT)
+			b_player[0].state = A_PUNCH_L;
+		else
+			b_player[0].state = A_STAND;
 
+	}
+}
+
+void loadEnemies() {
+	enemy.numEnemies = randNum(2)+2;
+	int i;
+	for(i=0;i<enemy.numEnemies;i++) {
+		enemy.list[i].x = 10;
+		enemy.list[i].y = i*40+10;
+		enemy.list[i].id = 0;
+		enemy.list[i].frame = 0;
+		enemy.list[i].hp = 10+i;
 	}
 }
 
@@ -48,8 +67,8 @@ void drawPlayers() {
 		int frame_number = (b_player[i].frame++/(ANIMATION_SPEED*4))%2;
 
 		// find player's offset in sprite data and copy it to SPRITE_GFX
-		int frame = frame_number + b_player[i].action * SPRITES_PER_ACTION;
-		u8 *offset = (u8 *)battlesTiles + SPRITE_ENTRY_SIZE*(b_player[i].id) + frame * 32*32; // each frame = 32x32
+		int frame = frame_number + b_player[i].state * SPRITES_PER_ACTION;
+		u8 *offset = (u8 *)battlePlayerTiles + SPRITE_ENTRY_SIZE*(b_player[i].id) + frame * 32*32; // each frame = 32x32
 		dmaCopy(offset, SPRITE_GFX+(32*32/2)*i, 32*32);	// copy sprite to OAM
 
 		// set up sprite information/update x and y coordinates
@@ -58,6 +77,29 @@ void drawPlayers() {
 
 		oamMain.oamMemory[i].gfxIndex = i*4;	// set npc's sprite id
 		oamMain.oamMemory[i].isHidden = false;
+	}
+	oamUpdate(&oamMain);
+}
+
+//2 sets of 6 sprites, 2 frames each, each frame is 32*32
+#define ENEMY_SPRITE_OFFSET 2*6*2*32*32
+
+void drawEnemies() {
+	int i;
+	for(i=0; i<enemy.numEnemies; i++) {
+		int frame_number = (enemy.list[i].frame++/(ANIMATION_SPEED*4))%2;
+
+		// find player's offset in sprite data and copy it to SPRITE_GFX
+		int frame = frame_number + enemy.list[i].state * SPRITES_PER_ACTION;
+		u8 *offset = (u8 *)battlePlayerTiles + ENEMY_SPRITE_OFFSET + enemy.list[i].id*SPRITE_ENTRY_SIZE + frame*32*32; // each frame = 32x32
+		dmaCopy(offset, SPRITE_GFX+(i+3)*(32*32/2), 32*32);	// copy sprite to OAM
+
+		// set up sprite information/update x and y coordinates
+		oamMain.oamMemory[i+3].attribute[0] = ATTR0_NORMAL | ATTR0_TYPE_NORMAL | ATTR0_COLOR_256 | ATTR0_SQUARE	| OBJ_Y(enemy.list[i].y);
+		oamMain.oamMemory[i+3].attribute[1] = ATTR1_SIZE_32 | OBJ_X(enemy.list[i].x);
+
+		oamMain.oamMemory[i+3].gfxIndex = (i+3)*4;	// set npc's sprite id
+		oamMain.oamMemory[i+3].isHidden = false;
 	}
 	oamUpdate(&oamMain);
 }
--- source/battle.h
+++ source/battle.h
@@ -1,4 +1,3 @@
-
 typedef enum {
     A_STAND,
     A_WALK,
@@ -8,17 +7,33 @@ typedef enum {
     A_KICK_H
 } b_actions;
 
-
 typedef struct {
 	int id;			// 0 = first character, 1 = 2nd char, 2 = 3rd character
-	int action;		// kick low, kick high, punch left, punch right, standing, running
+	int state;		// kick low, kick high, punch left, punch right, standing, running
 	int frame;
 	int x;
 	int y;
 }BattlePlayer;
 
-BattlePlayer b_player[3];
+typedef struct {
+	int id;
+	int frame;
+	int state;
+	int x;
+	int y;
+	int hp;
+}BattleEnemy;
 
+typedef struct {
+	BattleEnemy list[6];
+	int numEnemies;
+}Enemy;
+
+
+BattlePlayer b_player[3];
+Enemy enemy;
 
 void startBattle();
+void loadEnemies();
 void drawPlayers();
+void drawEnemies();

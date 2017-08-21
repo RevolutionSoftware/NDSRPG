438b02a07cb44b567cb0cfc51360d0e8c1f5bd5f
Andreas Wilfer <andreas@wilfer.se>
2017-08-21 12:21:14 +0200

    Moved file to includes folder

---
+++ includes/texts/texts.h
@@ -0,0 +1,8 @@
+char text0[] = "This is the first text in the game!";
+char text1[] = "This is another sign, yay!";
+char text2[] = "Welcome to my house :)";
+char text3[] = "This has a new-\nline! :O";
+char npc1[] = "Wow, you found me!";
+char npc2[] = "I can't find my keys, did you steal them? Yes! It was YOU! You stole my keys, you jerk! Now give me a banana!\n...\nWhat? Why not?\n(What a cheapskate...)";
+
+char* text_list[] = {text0,text1,text2,text3,npc1,npc2};
